5e78b1b5eb0f9d36bdf6a398ddf47ab15ef59e90
Nur Muhammad Supyan <nmsupyan@gmail.com>
2023-11-28 16:11:39 +0700

    Upload Project

---
+++ src/dao/BarangDao.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package dao;
+
+import java.util.List;
+import model.Barang;
+
+public interface BarangDao {
+    public void save(Barang barang);
+    public void update(Barang barang);
+    public void delete(Barang barang);
+    public Barang getBarang(String idBarang);
+    public List<Barang> getBarangs();
+ 
+}
